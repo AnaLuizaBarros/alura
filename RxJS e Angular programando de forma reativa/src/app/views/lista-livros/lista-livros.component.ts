b0095b3b0368bdb5430a25e176913ab5265279cb
Ana Luiza <analuizagbarros@gmail.com>
2023-09-03 23:07:14 -0300

    aula 1- 3

--- RxJS e Angular programando de forma reativa/src/app/views/lista-livros/lista-livros.component.ts
+++ RxJS e Angular programando de forma reativa/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
-
+export class ListaLivrosComponent implements OnDestroy {
   listaLivros: [];
+  campoBusca: string = '';
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private service: LivroService) {}
 
+  buscarLivros() {
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (retornoApi) => console.log(retornoApi),
+      error: (erro) => console.error(erro),
+      complete: () => console.log('Observable Completado'),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
-
-
-
